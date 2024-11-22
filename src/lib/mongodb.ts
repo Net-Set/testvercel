476fc5fb92dcbb889fc140c169cd40e2c5e1bb6e
Net-Set <somilsomil25@gmail.com>
2024-11-23 01:47:27 +0530

    update test 5

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,42 +1,16 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
+let client: MongoClient | null = null;
+const uri = process.env.MONGODB_URI || "your-mongodb-uri";
+client = new MongoClient(uri);
+const clientPromise: Promise<MongoClient> = client.connect();
 
-declare global {
-  var _mongoClientPromise: Promise<MongoClient> | undefined;
-}
+import { Document } from "mongodb";
 
-const MONGODB_URI = process.env.MONGODB_URI;
-const MONGODB_DB = process.env.MONGODB_DB;
-
-
-
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-}
-
-if (!MONGODB_DB) {
-  throw new Error('Please define the MONGODB_DB environment variable inside .env.local');
-}
-
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
-if (process.env.NODE_ENV === "development") {
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(MONGODB_URI);
-    global._mongoClientPromise = client.connect();
-  }
-  clientPromise = global._mongoClientPromise;
-} else {
-  client = new MongoClient(MONGODB_URI);
-  clientPromise = client.connect();
-}
-
-import { Document } from 'mongodb';
-
-export async function getCollection<T extends Document>(collectionName: string) {
-  const client = await clientPromise;
-  const db = client.db(MONGODB_DB);
+export const getCollection = async <T extends Document>(collectionName: string) => {
+  const dbName = process.env.MONGODB_DB || "default-db";
+  const db = (await clientPromise).db(dbName);
   return db.collection<T>(collectionName);
-}
+};
 
 export default clientPromise;
